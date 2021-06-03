4450e877c9a005df50b0cc6072291be3c1c9a64e
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-06-03 16:53:42 +0200

    booking page

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,9 +2,9 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr("#range_start", {
+  flatpickr(".datepicker", {
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end" })]
+    allowInput: true,
   });
 }
 
