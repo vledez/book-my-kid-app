324e02c33105a5607b974e2c908267b0c534b74e
theophile cestaro <theophilecestaro96@gmail.com>
2021-06-04 14:17:50 +0200

    nothing to change

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -6,15 +6,14 @@ class Kid < ApplicationRecord
   geocoded_by :localisation
   after_validation :geocode, if: :will_save_change_to_localisation?
   validates :name, presence: true
-<<<<<<< HEAD
 
-=======
+
+
   validates :localisation, presence: true
   validates :age, presence: true
   validates :price, presence: true
-  
+
   include PgSearch::Model
   pg_search_scope :search, against: [ :localisation, :service ], using: { tsearch: { prefix: true } }
-  
->>>>>>> master
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
-class Skill < ApplicationRecord
-  belongs_to :kid
-  belongs_to :booking
-end
+ # class Skill < ApplicationRecord
+ #   belongs_to :kid
+ #   belongs_to :booking
+ # end
